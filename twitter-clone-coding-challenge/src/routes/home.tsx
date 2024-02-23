d288d05cd290d50f98d0549ef493444fd025a811
hshk1004 <hshk_1004@naver.com>
2024-02-24 05:32:36 +0900

    Assignment 3
    making realtime posting

--- twitter-clone-coding-challenge/src/routes/home.tsx
+++ twitter-clone-coding-challenge/src/routes/home.tsx
@@ -1,12 +1,23 @@
+import styled from "styled-components";
 import { auth } from "../firebase";
+import PostTweetForm from "../components/tweet_posting-form";
+import Timeline from "../components/timeline";
+
+const Wrapper = styled.div`
+    display: grid;
+    gap: 50px;
+    overflow-y: scroll;
+    grid-template-rows: 1fr 5fr;
+`;
 
 export default function Home(){
     const logout = () => {
         auth.signOut();
     };
     return (
-        <h1>
-            <button onClick={logout}>Log Out</button>
-        </h1>
+        <Wrapper>
+            <PostTweetForm />
+            <Timeline />
+        </Wrapper>
     );
 }
